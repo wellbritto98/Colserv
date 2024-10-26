f4ec82ace52dcbea119b84f551f4399e99afe198
Kauã Tojal <150505239+kauatojal@users.noreply.github.com>
2024-10-26 11:20:23 -0300

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,7 +5,12 @@ const prisma = require('../db'); // Importar a instância do Prisma
 const registerIpcHandlers = () => {
   ipcMain.handle('register-user', async (event, userData) => {
     try {
-      const { name, email, password, cpf, phone, address, city, state, zip } = userData;
+      const { userRole, name, email, password, cpf, phone, address, city, state, zip, role } = userData;
+
+      // Verificar se o usuário é ADMIN antes de permitir o registro
+      if (userRole !== 'ADMIN') {
+        throw new Error('Permissão negada: apenas usuários ADMIN podem registrar novos usuários');
+      }
 
       // Verificar se o email ou CPF já está cadastrado
       const existingUser = await prisma.user.findUnique({ where: { email } });
@@ -17,7 +22,7 @@ const registerIpcHandlers = () => {
       // Hash da senha
       const hashedPassword = await bcrypt.hash(password, 10);
 
-      // Criar o usuário no banco de dados
+      // Criar o usuário no banco de dados com o campo de cargo
       const newUser = await prisma.user.create({
         data: {
           name,
@@ -29,10 +34,11 @@ const registerIpcHandlers = () => {
           city,
           state,
           zip,
+          role, // Adicionando o campo 'role'
         },
       });
 
-      return { id: newUser.id, name: newUser.name, email: newUser.email };
+      return { id: newUser.id, name: newUser.name, email: newUser.email, role: newUser.role };
     } catch (error) {
       console.error('Erro ao cadastrar usuário:', error);
       throw error;
