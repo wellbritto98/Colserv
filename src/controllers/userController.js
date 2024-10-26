19bec1b40a0f03f7275ad5bde0cafc55be91b4af
Jairo Neto <jairo.neto.dev@gmail.com>
2024-10-26 13:50:07 -0300

    feat: adicionado campo role no controller/newUser

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,17 +1,18 @@
-const { ipcMain } = require('electron');
-const bcrypt = require('bcrypt');
-const prisma = require('../db'); // Importar a instância do Prisma
+const { ipcMain } = require("electron");
+const bcrypt = require("bcrypt");
+const prisma = require("../db"); // Importar a instância do Prisma
 
 const registerIpcHandlers = () => {
-  ipcMain.handle('register-user', async (event, userData) => {
+  ipcMain.handle("register-user", async (event, userData) => {
     try {
-      const { name, email, password, cpf, phone, address, city, state, zip } = userData;
+      const { name, email, password, cpf, phone, address, city, state, zip } =
+        userData;
 
       // Verificar se o email ou CPF já está cadastrado
       const existingUser = await prisma.user.findUnique({ where: { email } });
       const existingCpf = await prisma.user.findUnique({ where: { cpf } });
       if (existingUser || existingCpf) {
-        throw new Error('Email ou CPF já cadastrado');
+        throw new Error("Email ou CPF já cadastrado");
       }
 
       // Hash da senha
@@ -20,6 +21,7 @@ const registerIpcHandlers = () => {
       // Criar o usuário no banco de dados
       const newUser = await prisma.user.create({
         data: {
+          role,
           name,
           email,
           password: hashedPassword,
@@ -34,26 +36,26 @@ const registerIpcHandlers = () => {
 
       return { id: newUser.id, name: newUser.name, email: newUser.email };
     } catch (error) {
-      console.error('Erro ao cadastrar usuário:', error);
+      console.error("Erro ao cadastrar usuário:", error);
       throw error;
     }
   });
 
-  ipcMain.handle('login-user', async (event, { email, password }) => {
+  ipcMain.handle("login-user", async (event, { email, password }) => {
     try {
       const user = await prisma.user.findUnique({ where: { email } });
       if (!user) {
-        throw new Error('Usuário não encontrado');
+        throw new Error("Usuário não encontrado");
       }
 
       const isPasswordValid = await bcrypt.compare(password, user.password);
       if (!isPasswordValid) {
-        throw new Error('Senha incorreta');
+        throw new Error("Senha incorreta");
       }
 
       return { id: user.id, name: user.name, email: user.email };
     } catch (error) {
-      console.error('Erro ao logar usuário:', error);
+      console.error("Erro ao logar usuário:", error);
       throw error;
     }
   });
