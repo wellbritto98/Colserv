3672a58f89388f6f78f98801e98c214c00d1b744
AlissoNNunes1 <alissonandradennnunes@gmail.com>
2024-10-26 11:16:57 -0300

    Login Validation

--- src/preload.js
+++ src/preload.js
@@ -2,4 +2,5 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
   registerUser: (userData) => ipcRenderer.invoke('register-user', userData),
+  loginUser: (userData) => ipcRenderer.invoke('login-user', userData),
 });
