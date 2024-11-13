52921caf12724ae68c00e4e507d482ca2fdb93c7
Lucas <luccas.leaal@gmail.com>
2024-11-13 20:55:34 -0300

    #07 - Migration criado

---
+++ prisma/migrations/20241113234612_chamado/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Chamado" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "titulo" TEXT NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "imagem" TEXT NOT NULL
+);
