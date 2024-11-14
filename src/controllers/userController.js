93ead01339fa6d3bac6809ab1c9f37feba1f3746
wellbritto98 <eng.jwellbritto@gmail.com>
2024-11-14 15:47:25 -0300

    ajuste

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -75,7 +75,7 @@ const registerIpcHandlers = () => {
           city: true,
           state: true,
           zip: true,
-          role: true, // Adicione o campo role aqui
+          //role: true, // Adicione o campo role aqui
         },
       });
       return users;
